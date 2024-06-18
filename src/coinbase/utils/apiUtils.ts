7c3b0d80f814d8bc343d731f7881cd00cddcdde8
Xellos1010 <xellos1010@gmail.com>
2024-06-18 15:09:24 -0400

    Reduced redundant code in API utils

--- src/coinbase/utils/apiUtils.ts
+++ src/coinbase/utils/apiUtils.ts
@@ -1,52 +1,58 @@
 // src/coinbase/utils/apiUtils.ts
-import axios from 'axios';
-import { generateJWT } from '../auth/generateJWT';
-import { config } from "../config";  // Adjust the import path as necessary
-const { baseUrl} = config;
+import axios from "axios";
+import { generateJWT } from "../auth/generateJWT";
+import { config } from "../config"; // Adjust the import path as necessary
+const { baseUrl } = config;
 
-const getRequest = async (path: string) => {
+const executeRequest = async (method: string, path: string, data?: object) => {
   try {
-    const token = generateJWT("GET", path);
+    const token = generateJWT(method, path);
     console.log("export JWT=" + token);
     console.log(
       `curl --ssl-no-revoke -H "Authorization: Bearer ${token}" https://${baseUrl}${path}`
     );
-    const response = await axios.get(
-      `https://${baseUrl}${path}`,
-      {
+
+    let response;
+    if (method === "GET") {
+      response = await axios.get(`https://${baseUrl}${path}`, {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
-      }
-    );
-    console.log("Fetched accounts data:", response.data);
-    return response.data;
+      });
+    } else if (method === "POST") {
+      response = await axios.post(`https://${baseUrl}${path}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    console.log(`Fetched ${method} data:`, response?.data);
+    return response?.data;
   } catch (error) {
     if (error instanceof Error) {
-      console.error("Error calling Coinbase API:", error.message);
+      console.error(`Error calling Coinbase API:`, error.message);
     } else {
       console.error("Unexpected error:", error);
     }
-    return error;
+    throw error; // re-throw the error after logging it
   }
 };
 
-// const postRequest = async (path: string, data: object) => {
-//   const token = generateJWT('POST', path);
-//   console.log(`curl --ssl-no-revoke -H "Authorization: Bearer ${token}" https://api.coinbase.com/api/v3/brokerage${path}`);
-//   const response = await apiClient.post(path, data, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-//   return response.data;
-// };
+const getRequest = async (path: string) => {
+  return await executeRequest("GET", path);
+};
+
+const postRequest = async (path: string, data: object) => {
+  return await executeRequest("POST", path, data);
+};
 
 // Function for integrated testing
 const getAccounts = async () => {
-  const path = '/accounts';
+  const path = "/accounts";
   return await getRequest(path);
 };
 
-export { getRequest, getAccounts };
+export { getRequest, postRequest, getAccounts };
